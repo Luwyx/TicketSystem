9d80841dc71aa5398597d6caccdd006d29156398
Gayvery <lokelindolsen@gmail.com>
2024-06-04 11:05:14 +0200

    Initial setup

--- TicketSystem/src/routes/+page.svelte
+++ TicketSystem/src/routes/+page.svelte
@@ -1,2 +1,20 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<!-- Javascript goes here -->
+<script>
+</script>
+
+<!-- HTML goes here -->
+<main>
+	<body>
+		<h1>Welcome to SvelteKit</h1>
+		<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+	</body>
+</main>
+
+<!-- CSS goes here -->
+<style>
+	* {
+		box-sizing: content-box;
+		margin: 0;
+		padding: 0;
+	}
+</style>
