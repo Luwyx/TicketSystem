ac47a15f4512301f343fe0382fad43be513acb52
Gayvery <lokelindolsen@gmail.com>
2024-06-06 11:14:25 +0200

    Ui Start

---
+++ TicketSystem/src/lib/navbar.svelte
@@ -0,0 +1,53 @@
+<div class="Sidebar">
+	<h1>Fovo Fynsk</h1>
+	<nav>
+		<a href="/User">User</a>
+		<a href="/tickets">Tickets</a>
+		<a href="/Sla">SLA</a>
+		<a href="/Faq">FAQ</a>
+	</nav>
+</div>
+
+<style>
+	@import url('https://fonts.googleapis.com/css2?family=Space+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap');
+
+	.Sidebar {
+		background-color: #333;
+		height: 100vh;
+		margin: 0;
+	}
+
+	nav {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		gap: 15%;
+		/* padding: 0 100px 0; */
+		height: max-content;
+	}
+
+	a {
+		color: white;
+		cursor: pointer;
+		font-family: 'Space mono', monospace;
+		font-weight: 400;
+		font-style: normal;
+		font-size: large;
+		text-decoration: none;
+		padding: 5px;
+	}
+
+	a:hover {
+		color: black;
+		background-color: white;
+		border-radius: 5px;
+	}
+
+	h1 {
+		color: white;
+		font-family: 'Space mono', monospace;
+		font-weight: 700;
+		font-size: 30px;
+	}
+</style>
---
+++ TicketSystem/src/routes/+layout.svelte
@@ -0,0 +1,39 @@
+<script>
+	import Navbar from '../lib/navbar.svelte';
+</script>
+
+<div class="grid-container">
+	<div class="topbar">
+		<p>Sidebar content here</p>
+	</div>
+	<div class="sidebar">
+		<Navbar />
+	</div>
+	<div class="content">
+		<slot />
+	</div>
+</div>
+
+<style>
+	.grid-container {
+		display: grid;
+		grid-template-rows: 15vh 1fr;
+		grid-template-columns: 20vw 1fr;
+		min-height: 100vh;
+	}
+	.topbar {
+		grid-row: 1;
+		grid-column: 2;
+	}
+	.sidebar {
+		grid-row: 1 / span 2;
+		grid-column: 1;
+		/* background-color: #f4f4f4; */
+		/* padding: 1rem; */
+	}
+	.content {
+		grid-row: 2;
+		grid-column: 2;
+		padding: 1rem;
+	}
+</style>
