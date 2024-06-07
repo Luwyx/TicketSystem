15fed150aa27457daa64e5f3af81f62aca18cf86
Gayvery <lokelindolsen@gmail.com>
2024-06-07 11:32:20 +0200

    Det ik virk

--- TicketSystem/src/routes/sla/+page.js
+++ TicketSystem/src/routes/sla/+page.js
@@ -1,10 +1,9 @@
 export async function load({ fetch }) {
-	const response = await fetch('../api/sla/');
+	const response = await fetch('/api/sla'); // Your API endpoint
 	const data = await response.json();
-
+	console.log('Fetched data in +page.js:', data);
+	console.log('test: ', data.header);
 	return {
-		props: {
-			data
-		}
+		data // Directly return the data object
 	};
 }
--- TicketSystem/src/routes/sla/+page.svelte
+++ TicketSystem/src/routes/sla/+page.svelte
@@ -5,7 +5,7 @@
 {#if data}
 	<h2>{data.header}</h2>
 	<p>{data.text}</p>
-	<p>{data.createData}</p>
+	<p>{data.creatData}</p>
 	<p>{data.version}</p>
 {:else}
 	<p>No data available</p>
