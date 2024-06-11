59904222ce5c6a310506e89a8d2622666543d480
Gayvery <lokelindolsen@gmail.com>
2024-06-11 12:43:17 +0200

    user styling

--- TicketSystem/src/routes/user/+page.svelte
+++ TicketSystem/src/routes/user/+page.svelte
@@ -12,7 +12,7 @@
 				throw new Error('Network response was not ok');
 			}
 			const tempData = await response.json();
-			
+
 			data = tempData[0];
 			console.log(data);
 		} catch (err) {
@@ -27,51 +27,65 @@
 
 <!-- HTML goes here -->
 <main>
-    <body>
-        <div class="userspacer">
-            <div class="iconholder">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z"/></svg>
-                <!-- Placeholder for username -->
-            </div>
-            <div class="userinfo">
-                {#if data !== null}
-                <p>Name: {data.firstName + ' ' + data.lastName}</p>
-                <p>Email: {data.email}</p>
-                <p>Work Phone: {data.workPhone}</p>
-                <p>Private Phone: {data.privatePhone}</p>
-                {:else}
-                <p>Name: N/A</p>
-                <p>Email: N/A</p>
-                <p>Work Phone: N/A</p>
-                <p>Private Phone: N/A</p>
-                {/if}
-            </div>
-            <div>
-                <h2>Current Tickets!</h2>
-                {#if data !== null}
-                <h3>{data.total_open_tickets}</h3>
-                {:else}
-                <h3>N/A</h3>
-                {/if}
-            </div>
-        </div>
-    </body>
+	<body>
+		<div class="userspacer">
+			<div class="user">
+				<div class="iconholder">
+					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"
+						><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path
+							d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z"
+						/></svg
+					>
+				</div>
+				{#if data !== null}
+					<h2>{data.firstName + ' ' + data.lastName}</h2>
+				{:else}
+					<h2>Name: N/A</h2>
+				{/if}
+			</div>
+			<hr />
+			<div class="userinfo">
+				{#if data !== null}
+					<p>Email: {data.email}</p>
+					<p>Work Phone: {data.workPhone}</p>
+					<p>Private Phone: {data.privatePhone}</p>
+				{:else}
+					<p>Name: N/A</p>
+					<p>Email: N/A</p>
+					<p>Work Phone: N/A</p>
+					<p>Private Phone: N/A</p>
+				{/if}
+			</div>
+			<hr />
+			<div class="tickets">
+				<h3>Current Tickets:</h3>
+				{#if data !== null}
+					<h3>{data.total_open_tickets}</h3>
+				{:else}
+					<h3>N/A</h3>
+				{/if}
+			</div>
+		</div>
+	</body>
 </main>
-
 
 <!-- CSS goes here -->
 <style>
-
 	.userspacer {
 		display: flex;
 		flex-direction: column;
 	}
 
+	.user {
+		display: flex;
+		align-items: center;
+	}
 
 	.iconholder {
 		width: 5vw;
 	}
 
-
-
+	.tickets {
+		display: flex;
+	}
 </style>
