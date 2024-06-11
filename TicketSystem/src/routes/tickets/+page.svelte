1f9d9659cbbcdf6ca51d08954152b3c93261aec1
Gayvery <lokelindolsen@gmail.com>
2024-06-11 11:23:20 +0200

    Ticket start

--- TicketSystem/src/routes/tickets/+page.svelte
+++ TicketSystem/src/routes/tickets/+page.svelte
@@ -7,7 +7,20 @@
 	<body>
 		<h1>Welcome to Tickets</h1>
 		<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-		<div class="ticketFlex"></div>
+		<div class="ticketFlex">
+			<!-- Insert tickets here -->
+			<article>
+				<div class="content">
+					<h3>{data.header}</h3>
+					<p>{data.text}</p>
+					<p>{data.createData}</p>
+				</div>
+				<div class="info">
+					<p>{data.status}</p>
+					<p>{data.priority}</p>
+				</div>
+			</article>
+		</div>
 	</body>
 </main>
 
@@ -16,5 +29,33 @@
 	.ticketFlex {
 		display: flex;
 		flex-direction: column;
+	}
+
+	article {
+		display: flex;
+		gap: 5vw;
+	}
+
+	div {
+		display: flex;
+		flex-direction: column;
+		gap: 10px;
+	}
+
+	.content {
+		justify-content: space-evenly;
+	}
+
+	.info {
+		height: 100%;
+		justify-content: space-evenly;
+	}
+
+	.info p {
+		color: white;
+		background-color: #333;
+
+		border-radius: 5px;
+		padding: 5px;
 	}
 </style>
