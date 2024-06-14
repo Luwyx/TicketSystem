624c8eba403d91d34151f16af29f685de5084df7
MN-House4it <mn@house4it.com>
2024-06-14 09:20:47 +0200

    Make back button work

--- TicketSystem/src/routes/tickets/[id]/notes/+page.svelte
+++ TicketSystem/src/routes/tickets/[id]/notes/+page.svelte
@@ -4,6 +4,7 @@
 	import { page } from '$app/stores';
 	import { get } from 'svelte/store';
 	import { user } from '../../../../stores.js';
+	import { goto } from '$app/navigation';
 
 	let data = [];
 	let error = null;
@@ -158,7 +159,7 @@
 
 <!-- HTML goes here -->
 <main>
-	<button class="backButton">
+	<button class="backButton" on:click={() => goto(`/tickets`)}>
 		<svg class="topBarSvg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"
 			><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path
 				d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z"
